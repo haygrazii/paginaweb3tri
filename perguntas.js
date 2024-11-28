0ecefd61dd06ad8457a1413a49cacd6209c19393
haygrazii <164929054+haygrazii@users.noreply.github.com>
2024-11-28 11:23:27 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Química',
+    'A que temperatura a água ferve?',
+    '100°C é conhecido como o ponto de ebulição da água'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor e o maior país do mundo?',
+    'Veticano e Rússia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é função?',
+    'Uma função é o bloco e código que executa alguma tarefa'
+)
